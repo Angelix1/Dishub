c265f6904227248db8f4a51ac8f092ad721c0357
Alixy Miyuki <86276165+alixy-miyuki@users.noreply.github.com>
2021-07-10 01:31:43 +0700

    Added Edit Function
    Adding Edit Function For simplify when editing a message

--- index.js
+++ index.js
@@ -4,21 +4,34 @@ function Say(message, content)
 {
     message.channel.send(content)
 }
+
 function Re(message, content)
 {
     message.reply(content)
 }
+
 function LOG(content)
 {
     console.log(content)
 }
+
 function Lreply(message, content)
 {
     message.lineReply(content)
 }
+
+function Edit(msg, content, time) 
+{
+    setTimeout(() => 
+    {
+        msg.edit(content)
+    }, time)    
+}
+
 module.exports = {
     Say,
     Re,
     LOG,
-    Lreply
+    Lreply,
+    Edit
 }
